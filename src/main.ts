3a05a1d89d67e8b1ed4f7ab1690fbcfb6117b718
NtakirutimanaI <ntakirutimanai@users.noreply.github.com>
2026-05-22 12:16:30 -0700

    Enable CORS for frontend and add npm dev script alias

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.NODE_ENV === 'production' ? ['https://goalgrowfoundation-frontend.vercel.app'] : true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 5000);
 }
